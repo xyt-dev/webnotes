af32d9d1c3fa0bb6365db79328be4f3aa1c6c6bb
xyt-dev <417639959@qq.com>
2024-07-15 13:26:36 +0800

    update TextBox1

--- src/app/english/page.tsx
+++ src/app/english/page.tsx
@@ -5,11 +5,18 @@ export default function EnglishPage() {
     <div className={`w-[390px] md:w-[50%] mx-auto`}>
       
       <TextBox>
-        <Title1>阅读的逻辑</Title1>
-        <Title2>阅读的逻辑</Title2>
-        <Title3>阅读的逻辑</Title3>
+        <Title1><span className="text-pink-600 underline">阅读的逻辑</span></Title1>
+
+        <Title2>句间逻辑</Title2>
+
+        <Title3>Text1</Title3>
         <Para>
-          text
+            In the weeks and months that followed Mr. Hrist's sale, spending of any sort became deeply unfashinable. In the art
+          world that meant collectors stayed away from galleries and salerooms.
+        </Para>
+
+        <Title3>Text2</Title3>
+        <Para>
         </Para>
       </TextBox>
 
--- src/components/TextBox1.tsx
+++ src/components/TextBox1.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 import {/*  merienda, */ kalam /* , noto_sans_jp */, zen_maru_gothic,  } from "@/fonts/fonts"
 import './font.css'
 
-export function Title1({children}: {children: ReactNode}) {
+export function Title1({children}: {children?: ReactNode}) {
   return (
     <div className="w-[100%] text-center font-bold text-[1.8rem] underline underline-offset-8 mb-6">
       {children}
@@ -10,25 +10,25 @@ export function Title1({children}: {children: ReactNode}) {
   )
 }
 
-export function Title2({children}: {children: ReactNode}) {
+export function Title2({children}: {children?: ReactNode}) {
   return (
-    <div className="w-[100%] text-center font-bold text-[1.3rem] mb-2">
+    <div className="w-[100%] text-center font-bold text-[1.3rem] mb-3">
       {children}
     </div>
   )
 }
 
-export function Title3({children}: {children: ReactNode}) {
+export function Title3({children}: {children?: ReactNode}) {
   return (
-    <div className="w-[100%] text font-bold text-[1rem]">
+    <div className="w-[100%] text font-bold text-[1.1rem] mb-1">
       {children}
     </div>
   )
 }
 
-export function Paragraph({children}: {children: ReactNode}) {
+export function Paragraph({children}: {children?: ReactNode}) {
   return (
-    <span>&nbsp;&nbsp;{children}</span>
+    <span className="block mb-1">&nbsp;&nbsp;{children}</span>
   )
 }
 
