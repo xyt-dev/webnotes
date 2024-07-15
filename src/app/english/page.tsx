db8c83d427353f448a7c9973116133c00dc49e4c
xyt-dev <417639959@qq.com>
2024-07-15 12:59:31 +0800

    update TestBox1: add titles

--- src/app/english/page.tsx
+++ src/app/english/page.tsx
@@ -1,12 +1,15 @@
 import TextBox from "@/components/TextBox1"
-import { Paragraph as Para } from "@/components/TextBox1"
+import { Paragraph as Para, Title1, Title2, Title3 } from "@/components/TextBox1"
 export default function EnglishPage() {
   return (
     <div className={`w-[390px] md:w-[50%] mx-auto`}>
-
-      <TextBox title="Text1">
+      
+      <TextBox>
+        <Title1>阅读的逻辑</Title1>
+        <Title2>阅读的逻辑</Title2>
+        <Title3>阅读的逻辑</Title3>
         <Para>
-          Wow
+          text
         </Para>
       </TextBox>
 
--- src/components/TextBox1.tsx
+++ src/components/TextBox1.tsx
@@ -2,9 +2,25 @@ import { ReactNode } from "react"
 import {/*  merienda, */ kalam /* , noto_sans_jp */, zen_maru_gothic,  } from "@/fonts/fonts"
 import './font.css'
 
-function Title({children}: {children: ReactNode}) {
+export function Title1({children}: {children: ReactNode}) {
   return (
-  <div className="w-[100%] text-center font-bold text-[1.2rem]">
+    <div className="w-[100%] text-center font-bold text-[1.8rem] underline underline-offset-8 mb-6">
+      {children}
+    </div>
+  )
+}
+
+export function Title2({children}: {children: ReactNode}) {
+  return (
+    <div className="w-[100%] text-center font-bold text-[1.3rem] mb-2">
+      {children}
+    </div>
+  )
+}
+
+export function Title3({children}: {children: ReactNode}) {
+  return (
+    <div className="w-[100%] text font-bold text-[1rem]">
       {children}
     </div>
   )
@@ -16,10 +32,9 @@ export function Paragraph({children}: {children: ReactNode}) {
   )
 }
 
-export default function TextBox1({children, title}: {children?: ReactNode, title?: ReactNode}) {
+export default function TextBox1({children}: {children?: ReactNode}) {
   return (
     <div className={`p-5 border-[1px] shadow-custom bg-neutral-200/10 text-blue-800 max-w-[800px] mx-auto ${kalam.className} ${zen_maru_gothic.className} Kalam_Zen_maru_gothic`}>
-      <Title>{title}</Title>
       {children}
     </div>
   )
