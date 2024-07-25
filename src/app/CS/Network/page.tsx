1bba32d8fa3bdd4cd9e61745bb593ca54a57a03a
xyt-dev <417639959@qq.com>
2024-07-25 18:54:34 +0800

    CS page v1.0

--- src/app/CS/Network/page.tsx
+++ src/app/CS/Network/page.tsx
@@ -1,12 +1,6 @@
 export default function NetworkPage() {
   return (
-    <div className="prose daisy-prose max-w-[1200px] leading-[35px] mx-auto p-6">
-      <h1>对你的 HTML 增加组件类</h1>
-      <p>
-      布局， 大小， grids 间隔等等。所有这些可以用 Tailwind CSS 的工具类来调整
-阅读更多
-      btn-neutral
-      </p>
+    <div className="prose daisy-prose max-w-[1200px] leading-[35px] mx-auto sm:p-6">
     </div >
   )
 }
