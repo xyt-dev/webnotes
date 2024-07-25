bf2e0af6acee2c52a1221c89258fb8842ac541b9
xyt-dev <417639959@qq.com>
2024-07-25 18:01:21 +0800

    update

--- src/app/CS/Network/page.tsx
+++ src/app/CS/Network/page.tsx
@@ -1,8 +1,32 @@
 export default function NetworkPage() {
   return (
-    <div className="prose daisy-prose max-w-[1000px] mx-auto pt-6">
+    <div className="prose daisy-prose max-w-[1000px] leading-[35px] mx-auto pt-6">
       <h1>对你的 HTML 增加组件类</h1>
-      <p>布局，大小</p>
-    </div>
+      布局，大小
+      <p>"布局，大小"</p>
+      btn-neutral
+      <ul>
+        <li> <strong>HeHeHe</strong> </li>
+        <li> <p>hahahahah<br />hehehe </p> </li>
+        <li> ahah
+          <ol>
+            <li> 
+              哟哟哟
+              <ul>
+                <li> hehe </li>
+                <li> haha </li>
+              </ul>
+            </li>
+            <li> wow </li>
+          </ol>
+        </li>
+        <li> aaa
+          <ul>
+            <li> hehe </li>
+            <li> haha </li>
+          </ul>
+        </li>
+      </ul>
+    </div >
   )
 }
