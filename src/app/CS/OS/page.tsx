f8e137d5d19bdeae972b584eeb19d1d6550c558d
xyt-dev <417639959@qq.com>
2024-07-25 18:51:47 +0800

    update

--- src/app/CS/OS/page.tsx
+++ src/app/CS/OS/page.tsx
@@ -1,6 +1,6 @@
 export default function OSPage() {
   return (
-    <div className="prose daisy-prose max-w-[1000px] leading-[35px] mx-auto pt-6">
+    <div className="prose daisy-prose max-w-[1200px] leading-[35px] mx-auto p-6">
       <h1>对你的 HTML 增加组件类</h1>
       <p>布局，大小</p>
     </div>
