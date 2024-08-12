bfdb2f14e461a0e988f9f92dbda899ea17cffdd3
xyt-dev <417639959@qq.com>
2024-08-12 20:14:16 +0800

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
+  basePath,
+  assetPrefix,
+  output: "export",
 };
 
 export default nextConfig;
