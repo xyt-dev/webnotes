49e5ae870c0529b10f7bcdada1e468f7d1878913
xyt-dev <417639959@qq.com>
2024-10-02 00:06:31 +0800

    update

--- src/app/math/pages/Solve1000/LinearAlgebra1.tsx
+++ src/app/math/pages/Solve1000/LinearAlgebra1.tsx
@@ -78,7 +78,7 @@ export default function LinearAlgebra1() {
             </li>
             <li><strong>特解一定和齐次解线性无关, 反证法证明很简单</strong></li>
             <li className="font-bold">[262] (1) 向量组线性无关 <Latex>{`$\\Leftrightarrow$`}</Latex> 向量组表出零向量, 则系数一定全部为0 (即不存在不全为零的系数组合, 也就是解向量组成的矩阵的秩r等于向量个数n); 设系数反向推出线性无关<br />
-            (2) <Latex>{`$Ax=b~$的任意解向量$~\\eta^*~$可表示为: $~\\eta^*=\\eta+\\lambda_0\\xi_1+\\lambda_2\\xi_2+\\cdots+\\lambda_{n-r}\\xi_{n-r}$ (其中$~\\eta~$是特解, $~\\xi_{1\\cdots(n-r)}~$为基础解系)`}</Latex><br />此题背下来</li>
+              (2) <Latex>{`$Ax=b~$的任意解向量$~\\eta^*~$可表示为: $~\\eta^*=\\eta+\\lambda_0\\xi_1+\\lambda_2\\xi_2+\\cdots+\\lambda_{n-r}\\xi_{n-r}$ (其中$~\\eta~$是特解, $~\\xi_{1\\cdots(n-r)}~$为基础解系)`}</Latex><br />此题背下来</li>
             <li className="font-bold">[263] 不同特解相减得到对应齐次方程组的非零解</li>
             <li className="font-bold"><strong>[264] 非齐次方程组的线性无关解的个数等于 n-r+1; </strong><br />
               (1) 方法一: 行向量线性相关, 设系数直接求解. 方法二: 增广矩阵作初等行变换, 得到行阶梯矩阵<br />
@@ -92,12 +92,12 @@ export default function LinearAlgebra1() {
             <li>[267] <Latex>{`$S\\ge 3-1=2, r(A)=n-S\\le n-2$`}</Latex></li>
             <li className="font-bold">[268] 任意n个线性无关向量可表出<span className="underline underline-offset-[3px]">其构成的n为向量空间中的</span>任意n维向量; <br />通解由n个线性无关向量组成 -&gt; 表出通解的n个线性无关向量也可以表出其空间中任意n维向量, 其中构成通解的特解向量系数为1, 所以方程组的系数矩阵 乘 特解和齐次解所能表出的n维空间中的任意向量得到的都是 <Latex>{`$k\\vec{b}$`}</Latex>. <br />
               特别地, 对于秩等于1的系数矩阵任意 <Latex>{`$\\vec{x}$`}</Latex> 所处整个向量空间就是特解和齐次解系表出的向量空间, 即 <Latex>{`$A\\vec{x}=k\\vec{b}$`}</Latex>. (其实很好理解, 因为系数矩阵中各基向量都在一条直线上) <br />
-              所以可以发现, 
+              所以可以发现,
             </li>
             <li>[269] 增广矩阵带参数分情况讨论计算通解; <strong>系数矩阵相同共用齐次解</strong></li>
             <li className="font-bold">[270] <Latex>{`$r(A)=r(B)=r(\\begin{bmatrix} A \\\\ B \\end{bmatrix})$`}</Latex> <Latex>{`$\\Leftrightarrow$`}</Latex> A 和 B 中行向量所处空间维数相同, 且不包含对方无法表出的向量 <Latex>{`$\\Leftrightarrow$`}</Latex> 即 A 和 B 的行向量组能互相表出 <Latex>{`$\\Leftrightarrow$`}</Latex> A 和 B 的行向量组是等价向量组.
-                <br />此时存在矩阵 <Latex>{`$C, D$ 使 $A=DB, B=CA$`}</Latex>, 易知 <Latex>{`$A\\vec{x}=\\vec{0}, \\\\ B\\vec{x}=\\vec{0}, \\begin{bmatrix} A \\\\ B \\end{bmatrix}\\vec{x}=\\vec{0}$`}</Latex> 是同解方程组. (行变换不改变列向量的线性关系) <br />
-                如果系数矩阵 A 和 B 的列向量组是等价向量组, 不能说明其齐次方程组(右乘 <Latex>{`$\\vec{x}$`}</Latex>)同解, 因为其行向量组合并后秩可能增大, 但其转置后行向量组为等价向量组, 方程组同解.
+              <br />此时存在矩阵 <Latex>{`$C, D$ 使 $A=DB, B=CA$`}</Latex>, 易知 <Latex>{`$A\\vec{x}=\\vec{0}, \\\\ B\\vec{x}=\\vec{0}, \\begin{bmatrix} A \\\\ B \\end{bmatrix}\\vec{x}=\\vec{0}$`}</Latex> 是同解方程组. (行变换不改变列向量的线性关系) <br />
+              如果系数矩阵 A 和 B 的列向量组是等价向量组, 不能说明其齐次方程组(右乘 <Latex>{`$\\vec{x}$`}</Latex>)同解, 因为其行向量组合并后秩可能增大, 但其转置后行向量组为等价向量组, 方程组同解.
             </li>
             <li><strong>[271]</strong> 看行向量组是否可以<strong>相互</strong>线性表出(三秩是否相等); 抽象矩阵行变换只能<strong>左乘</strong>矩阵, 且<strong>一般只能乘矩阵不能"除"矩阵(需要矩阵元素可逆)</strong></li>
             <li><strong>[272]</strong> 看清楚是三个平面公共交点, 即没有一个点同时满足三个平面方程, 即讨论参数令方程组无解</li>
@@ -119,9 +119,9 @@ export default function LinearAlgebra1() {
               <a className="text-blue-500 hover:text-blue-700 no-underline" target="_blank" href="https://www.zhihu.com/question/22548386">相关证明</a>
             </li>
             <li className="font-bold">线性相关的特征向量特征值一定相同, 所以不同特征值的特征向量一定线性无关</li>
-            <li className="font-bold">实对称矩阵必能正交相似对角化.<Space width={2}/>
+            <li className="font-bold">实对称矩阵必能正交相似对角化.<Space width={2} />
               <a className="text-blue-500 hover:text-blue-700 no-underline" target="_blank" href="Html/Math/实对称矩阵相关证明.html">证明</a> <br />
-              实对称矩阵不同特征值对应的特征向量一定相正交.<Space width={2}/>证明: <br /><Latex>{`$A\\xi_1 = \\lambda_1\\xi_1 ~ A\\xi_2 = \\lambda_2\\xi_2, ~ \\xi_1^T A \\xi_2 = \\lambda_2 \\xi_1^T \\xi_2 = (A\\xi_1)^T\\xi_2 = \\lambda_1\\xi_1^T\\xi_2 \\\\$ 又 $ \\lambda_1\\neq\\lambda_2 \\Rightarrow \\xi_1^T\\xi_2 = 0$`}</Latex><br />
+              实对称矩阵不同特征值对应的特征向量一定相正交.<Space width={2} />证明: <br /><Latex>{`$A\\xi_1 = \\lambda_1\\xi_1 ~ A\\xi_2 = \\lambda_2\\xi_2, ~ \\xi_1^T A \\xi_2 = \\lambda_2 \\xi_1^T \\xi_2 = (A\\xi_1)^T\\xi_2 = \\lambda_1\\xi_1^T\\xi_2 \\\\$ 又 $ \\lambda_1\\neq\\lambda_2 \\Rightarrow \\xi_1^T\\xi_2 = 0$`}</Latex><br />
               同一特征值的线性无关特征向量当然可以不正交, 但能进行施密特正交化
             </li>
             <li className="font-bold">存在<span className="underline underline-offset-[3px]">可逆矩阵</span>  <Latex>{`$P$ 使得 $P^{-1}AP=B$`}</Latex> 则称 <Latex>{`$A, B$`}</Latex> 为相似矩阵 <br />
@@ -149,12 +149,17 @@ export default function LinearAlgebra1() {
             <li><strong>[277]</strong> 将题目所给线性无关向量组组合为矩阵 <Latex>{`$P$`}</Latex>, 且有关系 <Latex>{`$AP=PB \\Rightarrow A=PBP^{-1} \\Rightarrow |A-\\lambda E| = |B-\\lambda E|$`}</Latex>, 从而求出 <Latex>{`$A$`}</Latex> 的特征值</li>
             <li className="font-bold">矩阵合同: 对 n 阶方阵 A、B, 存在可逆矩阵C, 有 <Latex>{`$B=C^TAC$`}</Latex> , 则称A与B合同, 记为 <Latex>{`$A\\simeq B$`}</Latex>. <br />
               对于实对称矩阵, 合同变换实际上只是对二次型中 <Latex>{`$\\vec{x}$`}</Latex> 的基向量变换, 相当于对坐标轴进行旋转/伸缩, 不改变惯性指数.</li>
-            <li className="font-bold">惯性定理: 实对称矩阵合同 <Latex>{`$\\Leftrightarrow$`}</Latex> 正、负惯性指数相同. <br /> 
+            <li className="font-bold">惯性定理: 实对称矩阵合同 <Latex>{`$\\Leftrightarrow$`}</Latex> 正、负惯性指数相同. <br />
               由于实对称矩阵一定正交合同(同时也是相似)于对角矩阵, 所以<br />实对称矩阵合同 <Latex>{`$\\Leftrightarrow$`}</Latex> 正、负、零特征值数量相同</li>
             <li className="font-bold"><Latex>{`矩阵关系小结: (待增加正定矩阵) $\\\\ $ 矩阵等价 $\\Rightarrow$ 秩相同 $\\\\$ 矩阵合同 $\\Rightarrow$ 秩相同 $\\\\$ 实对称矩阵合同 $\\Rightarrow$ 秩 + 正负惯性指数 + 正、负、零特征值数量相同 $\\\\$ 矩阵相似 $\\Rightarrow$ 秩 + 所有特征值相同 $\\\\$ 实对称矩阵相似 $\\Rightarrow$ 秩 + 正负惯性指数 + 所有特征值相同`}</Latex></li>
             <li className="font-bold">对于秩为 1 的矩阵 A 有: <Latex>{`$tr(A)\\neq 0 \\Leftrightarrow A$ 能够相似对角化`}</Latex> </li>
-            <li className="font-bold">[278] 实对称矩阵一定能正交相似对角化, 由相似的传递性知: <Latex>{`实对称矩阵特征值相同 $\\Leftrightarrow$ 矩阵相似`}</Latex>. 所以实对称矩阵不相似则特征值一定不完全相同
+            <li className="font-bold">[278] 实对称矩阵一定能正交相似对角化, 由相似的传递性知: <Latex>{`实对称矩阵特征值相同 $\\Leftrightarrow$ 矩阵相似`}</Latex>. 所以实对称矩阵不相似则特征值一定不完全相同 </li>
+            <li>[279] 检查重特征值的代数重数和几何重数是否相等</li>
+            <li>[281] <strong>齐次方程组的 非零解 都是特征值 0 的特征向量</strong>.<br /> 方法一: 线性无关解向量构成矩阵 P 且可逆, 知 <Latex>{`$AP=B, A=BP^{-1}$`}</Latex> <br />
+              方法二: 注意题目相当于给出了所有特征值及其对应线性无关特征向量 <Latex>{`$A=P\\Lambda P^{-1}$`}</Latex>
             </li>
+            <li>[282] 伴随矩阵的特征值</li>
+            <li>[283] 特征值数量小于 n, 可以代入已知特征值判断重特征值, 还可以利用 tr(A) = 特征值之和判断</li>
           </ul>
         </div>
       </TextBox>
