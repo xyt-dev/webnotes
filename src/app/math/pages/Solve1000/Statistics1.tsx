78ac776288f7eb562a23bc92acd2c4998f6e6385
xyt-dev <417639959@qq.com>
2024-10-09 01:16:30 +0800

    update

--- src/app/math/pages/Solve1000/Statistics1.tsx
+++ src/app/math/pages/Solve1000/Statistics1.tsx
@@ -118,15 +118,29 @@ export default function Statistics1() {
             <li>[339] 加绝对值</li>
             <li><strong>[340] 几何概型的基本方法: 画图</strong></li>
             <li className="font-bold">[341] (背下来, 正态分布) 配方, 凑正态概率密度函数; 正态分布函数记为 <Latex>{`$\\Phi(x)$`}</Latex>, 并且 <Latex>{`$\\Phi(x)+\\Phi(-x)=1$`}</Latex></li>
+            <li className="font-bold">[342] 正态分布 <Latex>{`$\\rightarrow$`}</Latex> 标准正态分布 <Latex>{`$\\rightarrow$`}</Latex> 卡方分布; 注意: <Latex>{`$2X$`}</Latex> 不是两个独立同分布随机变量之和</li>
+            <li>[343] (1) 分情况计算概率, <strong>当讨论多个随机变量的组合时, 一定要同时注意各随机变量的独立性和取值范围</strong></li>
+            <li className="font-bold">[344, 345, 348] 求Cov(X,Y) <Space width={1} /> 方法一: <Latex>{`$Cov(X,Y)=\\frac{D(X+Y)-D(X)-D(Y)}{2}\\\\$`}</Latex>X、Y、X+Y 的分布均可知使用方法一 <br /> 方法二: <Latex>{`$Cov(X,Y)=E(XY)-E(X)E(Y)$`}</Latex></li>
+            <li className="font-bold">[346] <Latex>{`$Cov(aX+bY, X) = aDX + bCov(X, Y)$`}</Latex></li>
+            <li className="font-bold">[347] 二维正态分布的概率密度公式</li>
+            <li className="font-bold">两个正态分布的随机变量不相关 <Latex>{`$\\Leftrightarrow$`}</Latex> 两个随机变量相互独立 </li>
+            <li className="font-bold">[349] (此题背下来) (1) 组合随机变量算概率密度总结: 首先看独立性! <br /> 
+              离散型 + 离散型: 直接看分布律 <br />
+              连续型 + 连续型: 卷积公式 (注意投影范围) <br />
+              离散型 + 连续型: 全概率公式 + 连续型随机变量概率密度函数积分得到分布函数; 求导得整体概率密度 <br />
+              (2) 注意 Z 可以拆为 与 Y 独立的 X 和 Y 本身, 于是 <Latex>{`$Cov(Y,Z) = Cov(Y, XY) = E(XY^2) - E(Y)E(XY) = E(X)E(Y^2) - E(X)E(Y)^2 = E(X)(E(Y)^2 + D(Y)) - E(X)E(Y)^2 = E(X)D(Y)$`}</Latex>
+            </li>
             <li className="font-bold">
               中心极限定理: 对于任意分布的独立的随机变量 <Latex>{`$X_1, X_2, ~\\cdots, X_n$`}</Latex>, 如果各随机变量的方差均为有限正数，且满足林德伯格条件, 则 <Latex>{`$ n\\rightarrow \\infty \\\\ \\frac{(X_1 + X_2 + \\cdots + X_n) - (\\mu_1 + \\mu_2 + \\cdots + \\mu_n)}{\\sqrt{\\sigma_1^2 + \\sigma_2^2 + \\cdots + \\sigma_n^2}}$`}</Latex> 趋近于标准正态分布 <br />
               只需要知道 独立同分布(且方差为有限正数)的随机变量之和 满足中心极限定理.
             </li>
-            <li className="font-bold">[342] 正态分布 <Latex>{`$\\rightarrow$`}</Latex> 标准正态分布 <Latex>{`$\\rightarrow$`}</Latex> 卡方分布; 注意: <Latex>{`$2X$`}</Latex> 不是两个独立同分布随机变量之和</li>
-            <li>[343] (1) 分情况计算概率, <strong>当讨论多个随机变量的组合时, 一定要同时注意各随机变量的独立性和取值范围</strong></li>
-            <li className="font-bold">[344, 345] 求Cov(X,Y) <Space width={1} /> 方法一: <Latex>{`$Cov(X,Y)=\\frac{D(X+Y)-D(X)-D(Y)}{2}\\\\$`}</Latex>X、Y、X+Y 的分布均可知使用方法一 <br /> 方法二: <Latex>{`$Cov(X,Y)=E(XY)-E(X)E(Y)$`}</Latex></li>
-            <li className="font-bold">[346] <Latex>{`$Cov(aX+bY, X) = aDX + bCov(X, Y)$`}</Latex></li>
-            <li className="font-bold">[347] 二维正态分布的概率密度公式</li>
+            <li className="font-bold"><Latex>{`简单随机样本 $\\Rightarrow$ 独立同分布`}</Latex>, 各随机变量分布与总体 X(模板随机变量) 相同</li>
+            <li>[350~] 大数定理, 中心极限定理</li>
+            <li>[355] 切比雪夫不等式</li>
+            <li className="font-bold">大数定律的理解: <Latex>{`若 $ lim_{n\\rightarrow \\infty}\\sum_{i=1}^nEX_i$ 为有限值, 则 $\\sum_{i=1}^nX_i $ 是概率集中于有限值的正态分布的随机变量, 于是 $P\\{lim_{n\\rightarrow \\infty}\\frac{1}{n}\\sum_{i=1}^nX_i=0\\}=1 \\\\$
+              否则 $\\sum_{i=1}^nX_i$ 是概率集中于无穷 (期望为 n 的无穷大) 的正态分布的随机变量, 即使 $\\sum_{i=1}^nX_i$ 实际值偏离期望也集中于等价无穷大 (因为标准差是 n 的低阶无穷大($\\sqrt{n}$) ), 于是 $P\\{lim_{n\\rightarrow \\infty}\\frac{1}{n}\\sum_{i=1}^nX_i = \\frac{1}{n}\\sum_{i=1}^nEX_i\\}=1$
+            `}</Latex></li>
+            <li className="font-bold">卡方分布的方差证明: <Img src="Images/Math/卡方分布的方差证明.png" /></li>
           </ul>
         </div>
       </TextBox>
