db7b6d57ba25ed408eef11bccbed66e136ff803b
xyt-dev <417639959@qq.com>
2024-11-27 17:27:43 +0800

    fix: EngWord overflow

--- src/app/english/pages/Accumulation.tsx
+++ src/app/english/pages/Accumulation.tsx
@@ -1,16 +1,22 @@
 import EngWord from "@/components/EngWord"
 import wordList from "./EnglishWordList.json"
 const words : Record<string, Record<string, string[]>> = wordList
-function Word({children, word, highlight}:{children:React.ReactNode, word:string, highlight:string}) {
+function Word({children, word, highlight}:{children:React.ReactNode, word:string, highlight?:string}) {
   return (
     <EngWord word={word} wordInfo={words[word]} highlight={highlight}>{children}</EngWord>
   )
 }
 export default function Accumulation() {
   return (
-    <div className="prose daisy-prose max-w-full leading-[35px] mx-auto p-6 pr-12">
+    <div className="prose daisy-prose max-w-full leading-[35px] mx-auto p-6 mr-6">
       <h1 className="text-center">Accumulation</h1>
-      They have invested heavily in projects designed to change the way of using and <Word word="conserve" highlight="v 1">conserving</Word> natural energy.
+      <ul>
+        <li>They have invested heavily in projects designed to change the way of using and <Word word="conserve" highlight="v 1">conserving</Word> natural energy.</li>
+        <li>We must <Word word="conserve" highlight="v 0">conserve</Word> endangered meadow lands.</li>
+        <li>What right has one country to <Word word="dictate" highlight="v 0">dictate</Word> the environmental standards of another?</li>
+        <li>The boss <Word word="dictate" highlight="v 2">dictated</Word> a long and detailed report to his assistant.</li>
+        <li>It's an approach not <Word word="dictate" highlight="v 1">dictated</Word> by the confines of conventional large scale agriculture led by international corporations.</li>
+      </ul>
     </div>
   )
 }
--- src/components/EngWord.tsx
+++ src/components/EngWord.tsx
@@ -1,15 +1,39 @@
 import { useState, useRef, useEffect } from "react";
 
-export default function EngWord({ children, word, wordInfo, highlight }: {children: React.ReactNode, word: string, wordInfo: {[key:string]:string[]}, highlight: string}) {
+export default function EngWord({ children, word, wordInfo, highlight }: {children: React.ReactNode, word: string, wordInfo: {[key:string]:string[]}, highlight?: string}) {
   const [isVisible, setIsVisible] = useState(false);
+  const [style, setStyle] = useState({});
+
+  const ref = useRef<HTMLDivElement>(null); // 创建一个 ref 来引用弹出内容
+  const parentRef = useRef<HTMLDivElement>(null); // 创建一个 ref 来引用弹出内容的父元素
+  const updatePosition = () => {
+    if (ref.current && parentRef.current) {
+      const rect = ref.current.getBoundingClientRect(); // 获取元素的边界矩形
+      const parentRect = parentRef.current.getBoundingClientRect(); // 获取父元素的边界矩形
+      const viewportWidth = window.innerWidth; // 获取视口的宽度
+
+      // 计算元素的右侧位置
+      const elementWidth = rect.width
+      const elementSupposedRight = parentRect.left +  elementWidth;
+
+      // 计算新的 right 值
+      let newRight = Math.min(viewportWidth - elementSupposedRight, 0); // 确保 right 不超过视口右侧
+      let right = (newRight === 0 ? '100%' : `${elementWidth}px`); // 当前的 right 值
+      setStyle({
+        right: right,
+      });
+    }
+  };
 
   const setVisibility = () => {
     setIsVisible(true);
+    updatePosition();
   };
 
-  const ref = useRef<HTMLDivElement>(null); // 创建一个 ref 来引用弹出内容
- // 处理点击外部区域关闭弹出内容
   useEffect(() => {
+    updatePosition();
+
+    // 处理点击外部区域关闭弹出内容
     const handleClickOutside = (event: MouseEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         setIsVisible(false);
@@ -18,20 +42,27 @@ export default function EngWord({ children, word, wordInfo, highlight }: {childr
 
     // 添加事件监听器
     document.addEventListener('mousedown', handleClickOutside);
+    window.addEventListener('resize', updatePosition); // 监听窗口大小变化
+
     return () => {
       // 清理事件监听器
+      window.removeEventListener('resize', updatePosition); 
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [ref]);
+  });
 
-  const highlightWordClass = highlight.split(' ')[0]; // 词性
-  const highlightIndex = parseInt(highlight.split(' ')[1]); // 词义位置
+  let highlightWordClass = ""
+  let highlightIndex = -1;
+  if (highlight !== undefined) {
+    highlightWordClass = highlight.split(' ')[0]; // 词性
+    highlightIndex = parseInt(highlight.split(' ')[1]); // 词义位置
+  }
 
   return (
     <span className="relative" onClick={setVisibility}>
-      <span className="cursor-pointer text-blue-500">{children}</span>
+      <span className="cursor-pointer text-blue-500" ref={parentRef}>{children}</span>
       {isVisible && (
-        <div ref={ref} className="absolute top-full right-full translate-x-full translate-y-1 bg-base-100 border border-base-300 p-[0.6rem] pl-[1rem] rounded-md shadow z-10 whitespace-nowrap">
+        <div ref={ref} className="absolute top-full right-full translate-x-full translate-y-1 bg-base-100 border border-base-300 p-[0.6rem] pl-[1rem] rounded-md shadow z-10 whitespace-nowrap" style={style}>
           <strong>{word}</strong> <br />
           {Object.entries(wordInfo).map(([wordClass, definitions]) => (
             <span key={wordClass} className="mb-2">
