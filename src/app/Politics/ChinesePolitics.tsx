a265d6ceddc8a04041a8d16ecf83ceb7354ab3fe
xyt-dev <417639959@qq.com>
2024-11-30 13:25:08 +0800

    update

--- src/app/Politics/ChinesePolitics.tsx
+++ src/app/Politics/ChinesePolitics.tsx
@@ -3,12 +3,55 @@ export default function ChinesePolitics() {
     <div className="prose daisy-prose max-w-full leading-[35px] mx-auto p-6">
       <h2>政治</h2>
       <ul>
+        <li><strong>常见帽子: 
+          <ul>
+            <li>出发点、落脚点、根本目的、归宿(做这事是为了什么): 为了人 / 综合性最强说法(包含了最多的意思)</li>
+            <li>立足点(立足于什么去干): 靠自己, 独立自主, 轻松奋斗</li>
+            <li>前提: 祖国统一 / 社会稳定</li>
+            <li>根本保证: 党的领导 / 某些制度</li>
+            <li>重要保障: 法制 / 某些制度</li>
+            <li>战略支撑: 国防军队 / 科技创新</li>
+            <li><strong>主线: </strong><br />
+            <strong>经济 -&gt; 供给侧结构性改革</strong> <br />
+            <strong>改革 -&gt; 制度建设</strong> <br />
+            <strong>民族 -&gt; 筑牢中华民族共同体意识</strong> <br />
+            <strong>党建 -&gt; 加强党的执政能力建设, 先进性、纯洁性建设</strong> <br />
+            <strong>一带一路 -&gt; 互联互通</strong> <br />
+            <strong>外交 -&gt; 服务民族复兴、促进人类进步</strong> <br />
+            </li>
+          </ul></strong>
+        </li>
+        <li><strong>发展新质生产力:
+          <ul>
+            <li>基本内涵: 劳动者、劳动资料、劳动对象及其优化组合的跃升</li>
+            <ul>
+              <li>特点: 创新</li>
+              <li>关键: 质优</li>
+              <li>本质: 先进生产力</li>
+            </ul>
+            <li>核心要素(要做到什么关键要素): 科技创新</li>
+            <li>核心标志(外显的标识): 全要素生产率大幅提升</li>
+            <li>核心特征: 高科技、高效能、高质量</li>
+            <li>动力: 全面深化改革</li>
+          </ul></strong>
+        </li>
+        <li><strong>高质量发展: </strong> <br />
+          高质量发展是<strong>体现新发展理念</strong>的发展, 是<strong>创新为第一动力</strong>的发展. <br />
+          <strong>推动高质量发展的内在要求和重要着力点: 发展新质生产力</strong> <br />
+          <strong>社会主义基本经济制度 -&gt; 保障 -&gt; 高质量发展 -&gt; 保障 -&gt; 人民美好生活需要 </strong><br />
+        </li>
         <li>
           <strong>2024.7.15-7.18. 第二十届中央委员会议第三次全体会议[二十届三中全会], 主题是: [进一步全面深化改革], 推进中国式现代化. </strong>
           <blockquote className="m-1 font-bold">
+            1937. 六届六中全会: 纠正抗战初期右倾错误, 明确提出马克思主义中国化. <br />
             1978. 十一届三中全会: 改革开放 <br />
             2013. 十八届三中全会: 全面深化改革 <br />
           </blockquote>
+        </li> 
+        <li>
+          1929(第一次国共合作之后). 古田会议: 确立了党对军队绝对领导的原则 <br />
+          1935. 瓦窑堡会议: 批判了党内长期存在着的“左”倾主义，制定了<strong>抗日民族统一战线</strong>的策略方针 <br />
+          1937. 洛川会议: <strong>全面抗战路线(人民战争路线, 全民族抗战路线), 会议通过《抗日救国十大纲领》</strong> <br />
         </li>
         <li><strong>中华民族共同体的形成和发展的~</strong>
           <ul>
@@ -18,6 +61,19 @@ export default function ChinesePolitics() {
             <li>坚韧纽带: 各民族情感相亲</li>
             <li>强大力量: 各民族经济相依</li>
           </ul>
+        </li>
+        <li>政协: <br />
+          统揽政协工作的<strong>总纲: 新时代中国特色社会主义思想</strong> <br />
+          政协工作的<strong>中心环节: 加强思想政治引领、广泛凝聚共识</strong> <br />
+          协商民主的基础: 不断拓展协商方式和平台 <br />
+          群众路线的体现: 全面发展协商民主 <br />
+        </li>
+        <li><strong>嫦娥六号: 人类首次月球背面自动采样返回</strong></li>
+        <li>精神口号: <br />
+            五四精神: 爱国、进步、民主、科学 <br />
+            建党精神: 坚持真理、坚守理想、践行初心、担当使命 <br />
+            工匠精神: 执着专注、精益求精 <br />
+            <strong>黄埔精神: 爱国、革命 </strong><br />
         </li>
       </ul>
     </div>
