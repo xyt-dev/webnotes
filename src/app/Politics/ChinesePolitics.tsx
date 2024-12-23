15a54336850718433dbedd26868d9d317d902f0c
xyt-dev <417639959@qq.com>
2024-12-23 17:05:40 +0800

    update

--- src/app/Politics/ChinesePolitics.tsx
+++ src/app/Politics/ChinesePolitics.tsx
@@ -1,3 +1,9 @@
+function Img({src, width, align}: { src: string, width?: number, align?: string }) {
+  if (align !== "left") align = "mx-auto"
+  return (
+     <img className={`mt-1 mb-1 ${align}`} src={src} alt="image" width={width} />
+  ) 
+}
 export default function ChinesePolitics() {
   return (
     <div className="prose daisy-prose max-w-full leading-[35px] mx-auto p-6">
@@ -76,6 +82,74 @@ export default function ChinesePolitics() {
             <strong>黄埔精神: 爱国、革命 </strong><br />
         </li>
       </ul>
+      <hr />
+      <p>
+        <h3>改革 与 新质生产力 与 高质量发展</h3>
+        第一, 推动新质生产力加快发展是发展命题. (推动新质生产力才能高质量发展) <br />
+        1. 说明: 新质生产力是摆脱了<strong>传统的经济增长方式和生产力发展路径</strong>, <strong>创新起主导作用、符合新发展理念</strong>(创新、协调、绿色、开放、共享)的<strong>先进生产力质态</strong>.<br />
+        2. 引出高质量发展: 坚持高质量发展是新时代的硬道理, 而发展新质生产力又是推动高质量发展的<strong>内力(内在要求和重要着力点)</strong> (高新 新高)<br />
+        3. 过渡: 新时代以来, 高质量发展取得显著成效, 但制约因素仍大量存在.<br />
+        4. 总结意义: 所以只有发展新质生产力, 才能实现<strong>质的有效提升和量的合理增长</strong>, 才能更好<strong>满足人民日益增长的美好生活需要</strong>.<br />
+        第二, 推动新质生产力加快发展也是改革命题. (改革才能推动新质生产力的发展) <br />
+        1. 说明: 新质生产力<strong>特点是创新, 关键是质优, 本质是先进生产力</strong>. <br />
+        2. 引出生产关系: 而唯物史观又指出, <strong>生产关系必须同生产力</strong>的发展要求<strong>相适应</strong>. <br />
+        3. 过渡: 所以, 发展新质生产力就需要与之匹配的科学机制和与之相适应的发展环境. <br />
+        4. 引出改革意义: 所以需要<strong>以改革创新作为持久动能, 以进一步全面深化改革为动力</strong>; <strong>着力打通束缚新质生产力发展的卡点, 形成与之更相适应的生产关系.</strong> <br />
+
+        <h3>推进中国式现代化 为什么需要实现 高水平科技自立自强 <br />(为什么科学技术打头阵, 科技创新是必由之路)</h3>
+        1. 两新一高: 科技自立自强是<strong>完整、全面、准确</strong>贯彻<strong>新发展理念</strong>, 构建<strong>新发展格局</strong>, 推动<strong>高质量发展</strong>的<strong>本质要求</strong>. <br />
+        2. 国家安全: 科技自立自强是<strong>是应对风险挑战、维护国家安全的现实需要, 是国家的强盛之基、安全之要</strong>; 要加快实现高水平自立自强, 解决<strong>"卡脖子"</strong>问题, 才能<strong>把发展的主动权牢牢掌握在自己手中</strong>. <br />
+        3. 科技革命机遇: 科技自立自强是<strong>抢抓 新一轮科技革命和产业变革机遇 的必然选择</strong>. 当前, <strong>全球科技创新进入空前密集活跃期</strong>, 我们必须加快推进高水平科技自立自强, <br />
+        才能在<strong>全球科技竞争中抢先一步, 赢得国家发展和安全的主动权.</strong> <br />
+
+        <h3>为何: "发展才是硬道理" -&gt; "坚持高质量发展作为新时代的硬道理"</h3>
+        {Img({src: "Images/ChinesePolitics/发展才是硬道理.png", width: 1000, align: "left"})}
+
+        <h3>改革开放、进一步全面深化改革 与 中国式现代化</h3>
+        {Img({src: "Images/ChinesePolitics/改革与中国式现代化1.png", width: 1000, align: "left"})}
+        {Img({src: "Images/ChinesePolitics/改革与中国式现代化2.png", width: 1000, align: "left"})}
+        关键词句: <br />
+              <strong>11.3 18.3 20.3 全会</strong> <br />
+              <strong>改革开放只有进行时, 没有完成时</strong> <br />
+              <strong>开启新时期; 开创新局面</strong> <br />
+              <strong>改革开放是新时代发展的根本动力; 是重要法宝、关键一招</strong> <br />
+              <strong>局部探索、与时俱进 -&gt; 系统集成、全面深化 -&gt; 一脉相承、与时俱进</strong> <br />
+              <strong>制度保障、物质(基础)条件、精神力量、强大动力 <br /> </strong>
+              <strong>推动 生产力&生产关系、经济基础&上层建筑、社会发展&国家治理 更好相适应</strong>
+
+        <h3>中华传统美德</h3>
+        {Img({src: "Images/ChinesePolitics/发扬中华优秀传统美德.png", width: 600, align: "left"})}
+        1. 点题: ... 是中华传统美德. <br />
+        2. 是什么, 蕴含什么: <strong>中华传统美德是中华文化的精髓</strong>, 蕴含着丰富的<strong>思想道德资源</strong>. <br />
+        3. 具有怎样的价值: 在长期历史发展中, 中华传统美德已经深入到<strong>全民族的思维方式、价值观念、行为方式和风俗习惯</strong>之中, 具有重要的<strong>当代价值</strong>. <br />
+        <div className="pl-[1.9em]">
+          当代价值: <br />
+          3.1 为当代到的建设提供有益启发. <br />
+          3.2 为解决当代人类面临的道德难题提供重要启迪. <br />
+          3.3 为当代青年成长提供宝贵精神营养. <br />
+        </div>
+        4. 怎么做: 我们应当自觉继承和弘扬中华传统美德: <br />
+        <div className="pl-[1.9em]">
+          4.1 加强对中华优秀传统美德的<strong>挖掘</strong>和阐发, 努力实现中华传统美德的<strong>创造性转化和创造性发展</strong>. <br />
+          4.2 结合时代要求, 用中华传统美德<strong>滋养社会主义道德建设</strong>. <br />
+          4.3 依照是否 <strong>有利于推动中国特色社会主义事业、建设社会主义道德体系、培育和践行社会主义核心价值观的标准</strong>, <strong>充分彰显</strong>中华传统美德的<strong>当代价值</strong>. <br />
+        </div>
+
+        <h3>青年高尚人生追求: 服务人民、奉献社会</h3>
+        {Img({src: "Images/ChinesePolitics/高尚人生追求.png", width: 1000, align: "left"})}
+
+        <h3>人类命运共同体; 文明交流、文明互鉴</h3>
+        先说人类命运共同体是什么、在引出题干("交流互鉴")同时解释题干、最后说明怎么做. <br />
+        {Img({src: "Images/ChinesePolitics/人类命运共同体需要交流互鉴.png", width: 1000, align: "left"})}
+        关键词句: <br />
+        1. 构建人类命运共同体 是 共建安全和平、开放包容、共同繁荣、清洁美丽的世界<br />
+        2. 需要和而不同、兼收并蓄的文明交流互鉴, 实现人类文明的创造性发展 <br />
+        3. 交流互鉴是文明发展、世界和平的本质要求和重要动力 <br />
+        4. 要践行全球文明倡议, 弘扬平等互鉴、对话包容的文明观 <br />
+
+        <h3>中国如何维护世界和平</h3>
+        {Img({src: "Images/ChinesePolitics/中国维护世界和平.png", width: 1000, align: "left"})}
+      </p>
     </div>
   )
 }
